cfdf9f789603c800bafc8875d8f65a73555e1f93
Marcin Orgacki <marcinorgacki@Marcins-MacBook-Pro.local>
2022-04-23 20:17:04 +0200

    fest: general update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import string
+from multiprocessing import Value
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
@@ -11,7 +11,7 @@ company_data_array = []
 
 def parse_html(driver: webdriver.Chrome):
     html = driver.page_source
-    subsite_html = BeautifulSoup(html, "html.parser")
+    subsite_html = BeautifulSoup(html, "html5lib")
     return subsite_html
 
 def login(username: str, password: str) -> None:
@@ -34,6 +34,9 @@ def parse_tiles(subsite_html: BeautifulSoup) -> list:
     job_tiles = subsite_html('div', {'class': "offer__info"})
     for job_tile in job_tiles:
         tile_link = job_tile('a', {'class': "offer-details__title-link"})
+        tile_link_multiple_cities = job_tile('button', {'class': "offer-details__title-link"})
+        wspolna_lista = []
+        #jak istnieje ('button', {'class': "offer-details__title-link"}) to trzeba wybrac pierwszy offer-regions__label i wbic w jego link ahref
         # for link in tiles_link:
         job_offer_url = tile_link[0].get('href')
         driver.get(job_offer_url)
@@ -50,10 +53,10 @@ def parse_tiles(subsite_html: BeautifulSoup) -> list:
             print("Nie pokryty przypadek przejscia na oferte")
         #sprawdzic czy przeszlo na konkretna ofercie jak nie to sprawdzic czy data-test='list-item-offer-location'
         #job offer view
-        driver.implicitly_wait(10)
+        WebDriverWait(driver=driver, timeout=10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "[data-test='anchor-apply']")))
         subsite_html = parse_html(driver=driver)
         driver.implicitly_wait(10)
-        apply_button = subsite_html.select("[data-test='anchor-apply']")[0]
+        apply_button = subsite_html.select_one("[data-test='anchor-apply']")
         driver.get(apply_button.get('href'))
         driver.implicitly_wait(10)
         #apply form view
@@ -63,12 +66,22 @@ def parse_tiles(subsite_html: BeautifulSoup) -> list:
             driver.get(job_offer_url)
             try:
                 #najpierw kliknac u gory w O firmie
+                view_company = driver.find_element(by=By.CSS_SELECTOR, value="[data-test='anchor-view-company']")
+                view_company.click() 
                 #potem kliknac w Zobacz pelny profil albo inne ... 
                 employer_profile = driver.find_element(by=By.CSS_SELECTOR, value="[data-test='button-employer-link']") 
                 employer_profile.click()
                 print("clicked!")
+
+                #nazwa-#div.default-box.contact-details > div > div.text > h3
+                #addres-#div.default-box.contact-details > div > div.text > [itemprop='address']
+                #nip-div.default-box.contact-details > div > div.text > [itemprop='taxID']
+                #itp-itd
             except:
-                print("NO BUTTON in this case", job_offer_url)
+                print()
+                with open('logs.txt', 'w') as f:
+                    f.write(f"'No button found on: ', {job_offer_url}")
+                    f.write('\n')
             #company details view
             aa_subsite_html = parse_html(driver=driver)
             try:
